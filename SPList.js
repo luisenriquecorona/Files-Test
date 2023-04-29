8e68fd8cc040d729807cd30f4fe9441029a1a738
TextKi JS <coronaenriquemorales@gmail.com>
2023-04-28 19:52:31 -0600

    Create SPList.js

---
+++ SPList.js
@@ -0,0 +1,36 @@
+var json = {
+  
+  xml1:
+  
+   {
+  'verb': 'createSPList',
+  'listName': 'Customer Tracking',
+  "templateType": 100,
+  "subactions": [
+    {
+      'verb': 'setDescription',
+      'description': 'List of Customers and Orders'
+    }
+  ]
+}
+
+};
+
+  
+var doct = {
+  
+  xml2:
+    
+   {   
+  "verb": "createSPList",
+  "listName": "Documents",
+  "templateType": 101,
+  "subactions": [
+    {
+      "verb": "setDescription",
+      "description": "This is a modified default document library"
+    }
+  ]
+}
+
+};
